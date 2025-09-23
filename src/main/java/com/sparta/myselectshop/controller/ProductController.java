1b14eef77479125a1d1739b11d5f20e039d493e8
Jess <jcscene@gmail.com>
2025-09-23 14:56:12 +0900

    feat: 관심 상품 희망 최저가 업데이트 API 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.sparta.myselectshop.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.service.ProductService;
@@ -22,9 +25,12 @@ public class ProductController {
 	public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
 
 		return productService.createProduct(requestDto);
-
 	}
 
+	@PutMapping("/products/{id}")
+	public ProductResponseDto updateProduct(@PathVariable("id") Long id, @RequestBody ProductMypriceRequestDto requestDto) {
 
+		return productService.updateProduct(id, requestDto);
+	}
 
 }
--- src/main/java/com/sparta/myselectshop/entity/Product.java
+++ src/main/java/com/sparta/myselectshop/entity/Product.java
@@ -1,5 +1,6 @@
 package com.sparta.myselectshop.entity;
 
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -33,10 +34,15 @@ public class Product extends Timestamped {
 	private int myprice;
 
 	public Product(ProductRequestDto requestDto) {
+
 		this.title = requestDto.getTitle();
 		this.image = requestDto.getImage();
 		this.link = requestDto.getLink();
 		this.lprice = requestDto.getLprice();
 	}
 
+	public void update(ProductMypriceRequestDto requestDto) {
+
+		this.myprice = requestDto.getMyprice();
+	}
 }
